a1ae3f6da6bb702dbe18b50e714cf040e8ebb3a1
Esther <esterdataengineer@gmail.com>
2022-05-03 20:26:13 +0300

    Created custom user model

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class Topping(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    # pizza = models.ForeignKey(
+    #      Pizza,
+    #     on_delete=models.CASCADE,
+    #     related_name='toppings'
+    # )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Pizza(models.Model):
     SIZE_CHOICES = [
         ('24 cm', '24 cm'),
@@ -10,15 +22,8 @@ class Pizza(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     price = models.DecimalField(decimal_places=2, max_digits=4, null=False, blank=False)
     size = models.CharField(max_length=255, null=False, blank=False, choices=SIZE_CHOICES)
-
-
-class Topping(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
-    pizza = models.ForeignKey(
-        Pizza,
-        on_delete=models.CASCADE,
-        related_name='toppings'
-    )
+    toppings = models.ManyToManyField(Topping, related_name="pizza_toppings")
+    image = models.FileField(upload_to="pizza_images/")
 
     def __str__(self):
         return f"{self.name}"
