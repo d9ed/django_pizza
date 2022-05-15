de601bf5af61f2c74cf2bc78c171e8198eccb668
Esther <esterdataengineer@gmail.com>
2022-05-15 20:06:13 +0300

    Created custom user model

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,8 +1,9 @@
 # from django.contrib import admin
 from django.urls import path, include
 
-from pizzas.views import PizzaListView
+from pizzas.views import PizzaListView, PizzaOrderView
 
 urlpatterns = [
     path('', PizzaListView.as_view(), name='pizzas'),
+    path('order/', PizzaOrderView.as_view(), name='order-pizza-form'),
 ]
