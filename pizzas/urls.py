5155383fa236b67bdff7509b3b725bbfbd0aac84
Esther <esterdataengineer@gmail.com>
2022-06-14 19:11:57 +0300

    Added select widget to pizza_detail

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,9 +1,10 @@
 # from django.contrib import admin
 from django.urls import path, include
 
-from pizzas.views import PizzaListView, PizzaOrderView
+from pizzas.views import PizzaListView, PizzaOrderView, PizzaDetailView
 
 urlpatterns = [
     path('', PizzaListView.as_view(), name='pizzas'),
     path('order/', PizzaOrderView.as_view(), name='order-pizza-form'),
+    path('detail/<int:pk>/', PizzaDetailView.as_view(), name='pizza-detail'),
 ]
